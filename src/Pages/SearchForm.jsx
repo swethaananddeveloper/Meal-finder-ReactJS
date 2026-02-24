f2ce20bb46395b886440a3e3b347f9179bd55cb8
Swetha <swetha.a1297@gmail.com>
2026-02-25 00:25:56 +0530

    search made clickable

--- src/Pages/SearchForm.jsx
+++ src/Pages/SearchForm.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { Link } from "react-router-dom"; // ✅ ADD THIS
+import { BiColor } from "react-icons/bi";
 
 const SearchForm = () => {
   const [foodName, setFoodName] = useState("");
@@ -16,8 +18,21 @@ const SearchForm = () => {
   };
 
   return (
-    <div className="container mt-5">
-      <form onSubmit={handleSubmit} className="d-flex justify-content-center mb-4">
+    <div className="container-fluid mt-5 py-5" 
+    style={{
+        backgroundImage:
+          "url('https://images.unsplash.com/photo-1499028344343-cd173ffc68a9')",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        minHeight: "20vh",
+      }}
+    >
+           <div className="container p-4 rounded shadow"> 
+      <form
+        onSubmit={handleSubmit}
+        className="d-flex justify-content-center mb-4"
+      >
         <input
           type="text"
           className="form-control w-50 me-2"
@@ -26,28 +41,33 @@ const SearchForm = () => {
         />
         <button className="btn btn-warning">Search</button>
       </form>
- <h2>What are your favorite cuisines?</h2>
-       <p>PERSONALIZE YOUR EXPERIENCE</p>
-   
+
+      <h2 className="text-white">What are your favorite cuisines?</h2>
+      <p className="text-white">PERSONALIZE YOUR EXPERIENCE</p>
+
       <div className="row">
         {results?.map((meal) => (
           <div className="col-md-3 mb-4" key={meal.idMeal}>
-            <div className="card">
-              <img
-                src={meal.strMealThumb}
-                className="card-img-top"
-                alt={meal.strMeal}
-              />
-              <div className="card-body text-center">
-                <h6>{meal.strMeal}</h6>
+            
+            <Link
+              to={`/meal/${meal.idMeal}`}
+              className="text-decoration-none text-dark"
+            >
+              <div className="card h-100 shadow-sm">
+                <img
+                  src={meal.strMealThumb}
+                  className="card-img-top"
+                  alt={meal.strMeal}
+                />
+                <div className="card-body text-center">
+                  <h6>{meal.strMeal}</h6>
+                </div>
               </div>
-            </div>
+            </Link>
           </div>
         ))}
-   
-         
       </div>
-       
+    </div>
     </div>
   );
 };
